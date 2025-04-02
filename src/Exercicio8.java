2f0b163e514f2b7068f5664b4f88e9e130ce1e36
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-01 22:21:57 -0300

    Exercicio 9

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Exercicio8 {
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int valor, dezena;
+
+        System.out.print(("Informe o valor com 3 digítos (100 - 999): "));
+        valor = sc.nextInt();
+
+        dezena = valor % 100 / 10;
+
+        System.out.println("Dezena é " + dezena);
+    }
 }
--- src/Exercicio9.java
+++ src/Exercicio9.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Exercicio9 {
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int digitado, primeiro, segundo, terceiro;
+
+        System.out.print("Digite um número 100-999: ");
+        digitado = sc.nextInt();
+
+        primeiro = digitado / 100;
+        segundo = digitado % 100 / 10;
+        terceiro = digitado % 10;
+
+        System.out.println("O valor ao contrário" + terceiro + segundo + primeiro
+        );
+
+    }
 }
